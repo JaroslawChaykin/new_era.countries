f7d7d065bb0f4a5ecacfbc240620ce356a8a6941
Jaroslaw <jaroslaw.chaikin@gmail.com>
2022-10-09 17:49:32 +0300

    change themes

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,55 @@
 <template>
-  <nav>
-    <router-link to="/">Go to Home</router-link>
-  </nav>
-  <router-view/>
+  <section class="main"
+           :class="theme">
+    <button @click="toggleTheme">Change theme</button>
+    <h1>hi</h1>
+    <nav>
+      <router-link to="/">Go to Home</router-link>
+    </nav>
+    <router-view/>
+  </section>
 </template>
 
 <script>
-
 export default {
   name: 'App',
-
-}
+  data() {
+    return {
+      theme: 'dark'
+    };
+  },
+  methods: {
+    toggleTheme() {
+      this.theme = this.theme === 'dark' ? 'white' : 'dark';
+    }
+  }
+};
 </script>
 
 <style>
 @import './assets/fonts/fonts.css';
+@import './assets/colors.css';
+
+.main {
+  width: 100%;
+  min-height: 100vh;
+  max-height: 100%;
+  background: var(--main-color);
+}
+
+button {
+  font-size: 72px;
+  text-transform: uppercase;
+  font-weight: 800;
+  background: var(--main-text-color);
+}
+
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
   font-family: 'NunitoSans', sans-serif, Arial;
+  font-size: 14px;
+  transition: 0.2s ease-in-out;
 }
 </style>
